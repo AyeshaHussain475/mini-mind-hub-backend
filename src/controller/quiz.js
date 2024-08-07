838ee00428a609d0d64b4b2100ea6412e8b67568
Ayesha Hussain <127660896+AyeshaHussain475@users.noreply.github.com>
2024-08-07 12:33:18 -0700

    implemented route for getting particular quiz questions

--- src/controller/quiz.js
+++ src/controller/quiz.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import Quiz from "../models/Quiz.js";
+import Question from "../models/Question.js";
 
 export const postQuiz = async (req, res) => {
   try {
@@ -44,6 +46,28 @@ export const getQuizzes = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       message: "Internal Sever error",
+      error,
+    });
+  }
+};
+
+export const getQuizQuestions = async (req, res) => {
+  try {
+    const { quizId } = req.params;
+
+    console.log(quizId);
+
+    if (!mongoose.Types.ObjectId.isValid(quizId)) {
+      return res.status(400).json("Quiz does not exists");
+    }
+
+    const questions = await Question.find({ quizId }).exec();
+    return res.json({
+      questions,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
       error,
     });
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { signin, signup } from "../controller/auth.js";
 import animalRouter from "./animal.js";
 import mediaRouter from "./media.js";
-import { getQuizzes, postQuiz } from "../controller/quiz.js";
+import { getQuizQuestions, getQuizzes, postQuiz } from "../controller/quiz.js";
 import { getQuestions, postQuestion } from "../controller/questions.js";
 
 const router = express.Router();
@@ -15,5 +15,6 @@ router.post("/postquiz", postQuiz);
 router.get("/getquizzes", getQuizzes);
 router.post("/postquestion", postQuestion);
 router.get("/getquestions", getQuestions);
+router.get("/getquizquestions/:quizId", getQuizQuestions);
 
 export default router;
